2e22f6f09143624302e5cf2c3c062a2b4a5bfd1b
comshin <79950083+comshin@users.noreply.github.com>
2023-04-26 11:14:54 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,6 +8,10 @@ chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
 st.line_chart(chart_data)
 
 

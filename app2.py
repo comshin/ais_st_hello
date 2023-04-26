ea58b0e870fccc06d8f060c3997eb84c6529c9cd
comshin <79950083+comshin@users.noreply.github.com>
2023-04-26 11:17:45 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -14,19 +14,7 @@ import streamlit as st
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
-'Starting a long computation...'
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
 
 st.line_chart(chart_data)
 
